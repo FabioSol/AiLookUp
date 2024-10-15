c4a73bc8c5edae8713147626161bd3d523eded1a
FabioSol <fabioso2231@gmail.com>
2024-10-15 01:13:01 -0600

    refactor

--- app/db/build.py
+++ app/db/build.py
@@ -9,6 +9,9 @@ def build():
     os.makedirs(data_path, exist_ok=True)
     os.makedirs(files_path, exist_ok=True)
 
+    with open(os.path.join(files_path, '.keep'), 'w') as keep_file:
+        keep_file.write('# This file is to keep the directory structure intact.\n')
+
     db = peewee.SqliteDatabase(data_path+'vectors.db')
     db.connect()
     if not db.table_exists('files'):
--- app/main.py
+++ app/main.py
@@ -1,20 +1,11 @@
 from PyQt5.QtWidgets import QApplication
-from app import MainWindow
+from app.app import MainWindow
 import sys
 import os
 
-from model.downloader import download
-from db.build import build
-
-
 if __name__ == "__main__":
-    if not os.path.exists('model/model'):
-        download()
-    if not os.path.exists('db/data'):
-        build()
-
     app = QApplication(sys.argv)
-    with open("static/style.qss", "r") as f:
+    with open("app/static/style.qss", "r") as f:
         stylesheet = f.read()
     app.setStyleSheet(stylesheet)
     window = MainWindow()
